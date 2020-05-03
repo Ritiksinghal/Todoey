d97622892d798f0cc6849137c31ba0ab62940950
Ritik Singhal <singhalritik00@gmail.com>
2020-05-04 04:43:08 +0530

    Final Commit

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -1,15 +1,15 @@
 //
-//  CategoryViewController.swift
+//  ViewController.swift
 //  Todoey
 //
-//  Created by Ritik on 30/04/20.
+//  Created by Ritik on 27/04/20.
 //  Copyright © 2020 Ritik. All rights reserved.
 //
-
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     
@@ -19,9 +19,18 @@ class CategoryViewController: UITableViewController {
         super.viewDidLoad()
         
         loadCategories()
+        
+        
     }
     
-    //MARK:- TableView Datasource Methods
+    override func viewWillAppear(_ animated: Bool) {
+        guard let navBar = navigationController?.navigationBar else { fatalError("Navigation controller does not exist.")
+        }
+        navBar.backgroundColor = UIColor(hexString: "0A84FF")
+    }
+    
+    
+    //MARK: - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories?.count ?? 1
@@ -29,79 +38,87 @@ class CategoryViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
-        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Category Added yet"
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet"
         
+        if let category = categories?[indexPath.row] {
+            guard let categoryColour = UIColor(hexString: category.colour) else {fatalError()}
+            cell.backgroundColor = categoryColour
+            cell.textLabel?.textColor = ContrastColorOf(categoryColour, returnFlat: true)
+        }
         return cell
     }
     
     
-    //MARK:- TableView Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        performSegue(withIdentifier: "goToItems", sender: self)
-        
-       func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            
-        let destinationVC = segue.destination as! TodoListViewController
-        
-        if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories?[indexPath.row]
-        }
-        }
-    }
-    
-    
-    //MARK:- Add New Categories
-    
-    
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-        var textField = UITextField()
-        
-        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            
-            let newCategory = Categories()
-            newCategory.name = textField.text!
-            
-            self.save(category: newCategory)
-        }
-        
-        alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Add a new category"
-            textField = alertTextField
-        }
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-        
-    }
-    
-    //MARK:- Data Manipulation Methods
-    
+    //MARK: - Data Manipulation Methods
     
     func save(category: Categories) {
-       
-        do{
+        do {
             try realm.write {
                 realm.add(category)
             }
         } catch {
-            print("Error saving context, \(error)")
+            print("Error saving category \(error)")
         }
-        
-        self.tableView.reloadData()
-    }
-    
-  func loadCategories() {
-   
-    categories = realm.objects(Categories.self)
-       
         tableView.reloadData()
     }
+    
+    func loadCategories() {
+        
+        categories = realm.objects(Categories.self)
+        tableView.reloadData()
+    }
+    
+    //MARK: - Delete Data from Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
+    
+    //MARK: - Add New Categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add a New Cateogry", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newCategory = Categories()
+            newCategory.name = textField.text!
+            newCategory.colour = UIColor.randomFlat().hexValue()
+            
+            self.save(category: newCategory)
+        }
+        
+        alert.addAction(action)
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories?[indexPath.row]
+        }
+    }
+    
+    
+    
 }
+
--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
     
     var todoItems: Results<Item>?
     let realm = try! Realm()
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var selectedCategory: Categories? {
         didSet{
@@ -25,6 +28,27 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        if let colourHex = selectedCategory?.colour {
+            
+            title = selectedCategory!.name
+            
+            guard let navBar = navigationController?.navigationBar else {
+                fatalError("Navigation controller does not exist.")
+            }
+            
+            if let navBarColour = UIColor(hexString: colourHex) {
+                
+                navBar.backgroundColor = navBarColour
+                
+                navBar.tintColor = ContrastColorOf(navBarColour, returnFlat: true)
+                
+                searchBar.barTintColor = navBarColour
+            }
+            
+        }
+    }
+    
     
     //MARK:- TableView Datasource methods
     
@@ -34,17 +58,21 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             
             cell.textLabel?.text = item.title
+            
+            if let colour = UIColor(hexString: selectedCategory!.colour)?.darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)) {
+                cell.backgroundColor = colour
+                cell.textLabel?.textColor = ContrastColorOf(colour, returnFlat: true)
+            }
+            
             cell.accessoryType = item.done ? .checkmark : .none
         } else{
             cell.textLabel?.text = "No Item Added"
         }
-        
-        
         
         return cell
     }
@@ -52,7 +80,7 @@ class TodoListViewController: UITableViewController {
     //MARK:- TableView delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      
+        
         if let item = todoItems?[indexPath.row] {
             
             do {
@@ -86,66 +114,76 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write {
                         let newItem = Item()
                         newItem.title = textField.text!
+                        newItem.dateCreated = Date()
                         currentCategory.items.append(newItem)
                     }
                 } catch {
                     print("Error saving Items, \(error)")
                 }
             }
-                
+            
             self.tableView.reloadData()
-                
-            }
             
-            alert.addTextField { (alertTextField) in
-                alertTextField.placeholder = "Create iew item"
-                textField = alertTextField
-                
-            }
-            
-            alert.addAction(action)
-            
-            present(alert, animated: true, completion: nil)
         }
         
-        //MARK:- Data manipulation methods
-        
-        
-        func loadItems() {
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create iew item"
+            textField = alertTextField
             
-            todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
-            
-            tableView.reloadData()
         }
         
-}
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK:- Data manipulation methods
+    
+    
+    func loadItems() {
+        
+        todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: - Delete Data from Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let itemForDeletion = self.todoItems?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(itemForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
+    
+}	
 
 //MARK:- Search bar methods
 
-/* extension TodoListViewController: UISearchBarDelegate {
- 
- func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
- 
- let request: NSFetchRequest<Item> = Item.fetchRequest()
- 
- let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
- 
- request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
- 
- loadItems(with: request, predicate: predicate)
- }
- 
- func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
- 
- if searchBar.text?.count == 0 {
- loadItems()
- 
- DispatchQueue.main.async {
- searchBar.resignFirstResponder()
- }
- }
- 
- }
- 
- } */
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+        
+    }
+    
+}
 
